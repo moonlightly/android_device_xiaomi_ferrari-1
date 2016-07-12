e48bf5c7357eaf89e1d889b155c2844078a21bd6
Haikal Izzuddin <webhaikal@gmail.com>
2016-07-12 15:29:20 +0800

    BoardConfid: Stop building libinit_msm

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,6 @@ LOCAL_PATH := device/xiaomi/ferrari
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Bootloader
